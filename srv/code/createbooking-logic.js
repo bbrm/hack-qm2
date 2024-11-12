e2556718652d830c8c785b42398af9e46d714298
Florian Lüscher <florian.luescher@axa.ch>
2024-11-12 23:34:12 +0000

    testing service

--- srv/code/createbooking-logic.js
+++ srv/code/createbooking-logic.js
@@ -4,11 +4,17 @@
  * @param {Object} request - User information, tenant-specific CDS model, headers and query parameters
 */
 module.exports = async function(request) {
+	console.log("lflo");
+	console.log(request.data);
+	
+	
 	let booking = {
-		employee_ID: request.employee_ID,
-		workplace_ID: request.workplace_ID,
-		date: request.date
+		employee_ID: request.data.employee_ID,
+		workplace_ID: request.data.workplace_ID,
+		date: request.data.date
 	};
+
+	// TODO Save Booking
 	
 	console.log(booking);
 
